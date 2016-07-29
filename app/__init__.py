6381f89be92298522a63c549d3c8e9defb38daf1
fonky <fonky@Debian.fonky>
2016-07-30 01:14:21 +0200

    db instance added

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config["MONGODB_SETINGS"] = {'DB': "flaskapp"}
+app.config["SECRET_KEY"] = "KeepThisS3cr3t"
 
+db = MongoEngine(app)
 
 if __name__ == '__main__':
 	app.run()
